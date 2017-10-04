8b75dd221c779a9d5b4c7e31dc09f2946db68966
CloseSu <closeSu@gmail.com>
2017-10-04 11:12:22 +0800

    20171004

---
+++ demo/src/main/java/com/example/demo/mediator/Colleague.java
@@ -0,0 +1,9 @@
+package com.example.demo.mediator;
+
+public class Colleague {
+	Mediator mediator;
+	
+	public Colleague(Mediator mediator) {
+		this.mediator = mediator;
+	}
+}
---
+++ demo/src/main/java/com/example/demo/mediator/Mediator.java
@@ -0,0 +1,5 @@
+package com.example.demo.mediator;
+
+public abstract class Mediator {
+	public abstract void send(String message,Colleague colleague);
+}
